fd592c8a1ea0705105868ae14a9e55a61eeea734
Adedipupo <ajokanola@terragonltd.com>
2022-11-08 18:44:38 +0100

    create user

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,16 +1,24 @@
 import express from "express";
+import asyncHandler from "express-async-handler"
 
 
-export const registerUser = (req, res) =>{
-    if(!req.body.email){
-         res.status(400)
-            throw new Error("Please provide an email")
+export const registerUser = asyncHandler(async(req, res) =>{
+    const {name, email, password} = req.body;
+    const userExists = await UserModel.findOne({email});
+    if(userExists){
+        res.status(400);
+        throw new Error("User already exists");
     }
-    const {username, email, password} = req.body
-    console.log(username, email, password)
-    res.send({ 
-        username,
+    const user = await UserModel.create({
+        name,
         email,
         password
     })
-}
+    if(user){
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+        })
+    }
+})
--- api/server.js
+++ api/server.js
@@ -5,6 +5,7 @@ import mongoose from 'mongoose'
 import bodyParser from 'body-parser'
 import homeRoutes from './routes/index.js'
 import userRoutes from './routes/userRoute.js'
+import errorHandler from './middleware/errorMiddleware.js'
 
 dotenv.config()
 
@@ -14,11 +15,20 @@ app.use(express.json())
 app.use(urlencoded({extended:false}))
 app.use(bodyParser.json())
 app.use(cors({
-    origin: 'http://localhost:3000',
-    credentials: true,
+  origin: '*',
+  credentials: true,
 }))
 
 const PORT = process.env.PORT || 1234
+
+app.get('/',(req, res)=>{
+  res.send("api is live...")
+});
+app.use('/api/users',userRoutes);
+app.use(errorHandler)
+
+
+
 
 mongoose
   .connect(process.env.MONGODB_URI)
@@ -26,10 +36,5 @@ mongoose
     console.log(`Connected to mongodb successfully,Server running on port ${PORT}`)
   })})
   .catch((err) => console.log(err))
-
-app.get('/',(req, res)=>{
-    res.send("api is live...")
-});
-app.use('/api/users',userRoutes);
 
 
