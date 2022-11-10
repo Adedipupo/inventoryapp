5b00b256b009a49c489b248c2a70dd2e75006d19
Adedipupo <ajokanola@terragonltd.com>
2022-11-10 17:06:59 +0100

    password encryption

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,24 +1,35 @@
 import express from "express";
 import asyncHandler from "express-async-handler"
+import { UserModel } from "../models/userModel.js";
 
 
 export const registerUser = asyncHandler(async(req, res) =>{
     const {name, email, password} = req.body;
+
+    if(!name || !email || !password){
+        res.status(400);
+        throw new Error("Please provide all fields");
+    }
+
     const userExists = await UserModel.findOne({email});
     if(userExists){
         res.status(400);
         throw new Error("User already exists");
     }
+
     const user = await UserModel.create({
         name,
         email,
         password
     })
     if(user){
+        const { _id,name,email,password,photo,phone,bio} = user;
         res.status(201).json({
-            _id: user._id,
-            name: user.name,
-            email: user.email,
+            _id,name,email,password,photo,phone,bio
         })
+    }else{
+        res.status(400);
+        throw new Error("Invalid user data");
     }
+
 })
--- api/models/userModel.js
+++ api/models/userModel.js
@@ -1,41 +1,51 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose'
+import bcrypt from 'bcryptjs'
 
-
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     name: {
-        type: String,
-        required: [true, "Please add a name"]
+      type: String,
+      required: [true, 'Please add a name'],
     },
     email: {
-        type: String,
-        required: [true, "Please add an email"],
-        unique: true,
-        trim: true,
-        match: [
-            /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,"Please add a valid email"
-        ]
+      type: String,
+      required: [true, 'Please add an email'],
+      unique: true,
+      trim: true,
+      match: [
+        /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+        'Please add a valid email',
+      ],
     },
     password: {
-        type: String,
-        required: [true, "Please add a password"],
+      type: String,
+      required: [true, 'Please add a password'],
     },
-    photo:{
-        type: String,
-        default: "no-photo.jpg"
+    photo: {
+      type: String,
+      default: 'no-photo.jpg',
     },
     phone: {
-        type: Number,
-        default: +234
+      type: Number,
+      default: +234,
     },
     bio: {
-        type: String,
-        default: "My Bio",
-        maxLength: [250, "Bio must not be more than 250 characters"]
-    }
-},{
+      type: String,
+      default: 'My Bio',
+      maxLength: [250, 'Bio must not be more than 250 characters'],
+    },
+  },
+  {
     timestamps: true,
-
-}
+  },
 )
 
-const UserModel = mongoose.model('User', userSchema)
+userSchema.pre("save",async function (next) {
+    if(!this.isModified("password")){
+        next();
+    }
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password,salt); 
+})
+
+export const UserModel = mongoose.model('User', userSchema)
