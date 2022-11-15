eb9274315e1b0b63612c3f5fa88e7284cadd4004
Adedipupo <ajokanola@terragonltd.com>
2022-11-15 09:59:35 +0100

    multer setup

--- api/controllers/productController.js
+++ api/controllers/productController.js
@@ -1,4 +1,5 @@
 import asyncHandler from "express-async-handler"
+import { ProductModel } from "../models/productModel.js";
 
 export const createProduct = asyncHandler(async (req, res) => {
     const {name,sku,category,quantity,price,description} = req.body;
@@ -7,4 +8,19 @@ export const createProduct = asyncHandler(async (req, res) => {
         res.status(400)
         throw new Error('Please provide all fields')
       }
+
+    const product = await ProductModel({
+        user: req.user.id,
+        name,
+        sku,
+        category,
+        quantity,
+        price,
+        description
+    })
+
+    res.status(201).json({
+        msg: 'Product created successfully',
+        data: product
+    })
 })
