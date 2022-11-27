6b335bf779009fe13cf728b540cff734227c08b5
Adedipupo <ajokanola@terragonltd.com>
2022-11-27 05:57:59 +0100

    wip

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,11 @@
 import { Routes, Route, Link } from "react-router-dom";
+import Layout from "./components/Layout/Layout";
+import Sidebar from "./components/Sidebar/Sidebar";
 import ForgotPassword from "./pages/Auth/ForgotPassword";
 import Login from "./pages/Auth/Login";
 import Register from "./pages/Auth/Register";
 import ResetPassword from "./pages/Auth/ResetPassword";
+import Dashboard from "./pages/Dashboard/Dashboard";
 import Home from "./pages/Home/Home";
 
 
@@ -15,6 +18,14 @@ function App() {
     <Route path="/register" element={<Register />} />
     <Route path="/forgotpassword" element={<ForgotPassword />} />
     <Route path="/resetpassword/:resetToken" element={<ResetPassword />} />
+
+    <Route path="/dashboard"  element={
+      <Sidebar>
+        <Layout>
+          <Dashboard />
+        </Layout>
+      </Sidebar>
+    }/>
   </Routes>
   );
 }
