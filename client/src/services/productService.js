6aa5892ce6f06898c5546f37f8a6cd7b0475193b
Adedipupo <ajokanola@terragonltd.com>
2022-12-03 07:05:08 +0100

    get-products-redux-function

--- client/src/services/productService.js
+++ client/src/services/productService.js
@@ -9,9 +9,14 @@ const createProduct = async (formData) => {
       const response = await axios.post(`${BASE_URL}/api/products/create`, formData)
       return response.data;  
   }
+const getProducts = async () => {
+      const response = await axios.get(`${BASE_URL}/api/products`)
+      return response.data;  
+  }
 
   const productService = {
-    createProduct 
+    createProduct,
+    getProducts
   }
 
   export default productService;
