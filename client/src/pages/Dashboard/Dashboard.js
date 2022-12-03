eaa4d26716a097b9334914a9a7a7a191bb9be44e
Adedipupo <ajokanola@terragonltd.com>
2022-12-03 18:57:42 +0100

    wip

--- client/src/pages/Dashboard/Dashboard.js
+++ client/src/pages/Dashboard/Dashboard.js
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 import useRedirectLoggedOutUser from '../../customHook/useRedirectLoggedOutUser'
+import { getProducts } from '../../redux/features/product/productSlice'
 
 const Dashboard = () => {
   useRedirectLoggedOutUser("/login")
+const dispatch = useDispatch()
+
+useEffect(() => {
+  const cccc = dispatch(getProducts())
+  console.log(cccc)
+},[])
+
   return (
     <div>Dashboard</div>
   )
