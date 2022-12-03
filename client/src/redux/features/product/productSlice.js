f8262ab483f59b628e2592768b591866ec93075a
Adedipupo <ajokanola@terragonltd.com>
2022-12-03 05:22:14 +0100

    add-product

--- client/src/redux/features/product/productSlice.js
+++ client/src/redux/features/product/productSlice.js
@@ -1,40 +1,124 @@
-import { createSlice,createAsyncThunk } from '@reduxjs/toolkit';
-import productService from '../../../services/productService';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import { toast } from 'react-toastify'
+import productService from '../../../services/productService'
 
 const initialState = {
-    product : null,
-    products : [],
-    isError : false,
-    isSuccess : false,
-    message : "" 
+  product: null,
+  products: [],
+  isError: false,
+  isSuccess: false,
+  message: '',
 }
 
 const createProduct = createAsyncThunk(
-    "product/create",
-    async (formData,thunkAPI) => {
-        try {
-            return await productService.createProduct(formData)
-        } catch (error) {
-            const message = (error.response && error.response.data && error.response.data.message) || error.message || error.toString();
-            console.log(message)
-            return thunkAPI.rejectWithValue(message)
-        }
+  'product/create',
+  async (formData, thunkAPI) => {
+    try {
+      return await productService.createProduct(formData)
+    } catch (error) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString()
+      console.log(message)
+      return thunkAPI.rejectWithValue(message)
     }
+  },
 )
 
 const productSlice = createSlice({
-    name: 'product',
-    initialState,
-    reducers: {
-        CALC_STORE_VALUE(state,action){
-            console.log("first")
-        }
+  name: 'product',
+  initialState,
+  reducers: {
+    CALC_STORE_VALUE(state, action) {
+      console.log('first')
     },
-    extraReducers: (builder) => {
-
-    }
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(createProduct.pending, (state) => {
+        state.isLoading = true
+      })
+      .addCase(createProduct.fulfilled, (state, action) => {
+        state.isLoading = false
+        state.isSuccess = true
+        state.isError = false
+        console.log(action.payload)
+        state.products.push(action.payload)
+        toast.success('Product added successfully')
+      })
+      .addCase(createProduct.rejected, (state, action) => {
+        state.isLoading = false
+        state.isError = true
+        state.message = action.payload
+        toast.error(action.payload)
+      })
+    //   .addCase(getProducts.pending, (state) => {
+    //     state.isLoading = true
+    //   })
+    //   .addCase(getProducts.fulfilled, (state, action) => {
+    //     state.isLoading = false
+    //     state.isSuccess = true
+    //     state.isError = false
+    //     console.log(action.payload)
+    //     state.products = action.payload
+    //   })
+    //   .addCase(getProducts.rejected, (state, action) => {
+    //     state.isLoading = false
+    //     state.isError = true
+    //     state.message = action.payload
+    //     toast.error(action.payload)
+    //   })
+    // .addCase(deleteProduct.pending, (state) => {
+    //     state.isLoading = true;
+    //   })
+    //   .addCase(deleteProduct.fulfilled, (state, action) => {
+    //     state.isLoading = false;
+    //     state.isSuccess = true;
+    //     state.isError = false;
+    //     toast.success("Product deleted successfully");
+    //   })
+    //   .addCase(deleteProduct.rejected, (state, action) => {
+    //     state.isLoading = false;
+    //     state.isError = true;
+    //     state.message = action.payload;
+    //     toast.error(action.payload);
+    //   })
+    //   .addCase(getProduct.pending, (state) => {
+    //     state.isLoading = true;
+    //   })
+    //   .addCase(getProduct.fulfilled, (state, action) => {
+    //     state.isLoading = false;
+    //     state.isSuccess = true;
+    //     state.isError = false;
+    //     state.product = action.payload;
+    //   })
+    //   .addCase(getProduct.rejected, (state, action) => {
+    //     state.isLoading = false;
+    //     state.isError = true;
+    //     state.message = action.payload;
+    //     toast.error(action.payload);
+    //   })
+    //   .addCase(updateProduct.pending, (state) => {
+    //     state.isLoading = true;
+    //   })
+    //   .addCase(updateProduct.fulfilled, (state, action) => {
+    //     state.isLoading = false;
+    //     state.isSuccess = true;
+    //     state.isError = false;
+    //     toast.success("Product updated successfully");
+    //   })
+    //   .addCase(updateProduct.rejected, (state, action) => {
+    //     state.isLoading = false;
+    //     state.isError = true;
+    //     state.message = action.payload;
+    //     toast.error(action.payload);
+    //   });
+  },
 })
 
-export const {CALC_STORE_VALUE} = productSlice.actions
+export const { CALC_STORE_VALUE } = productSlice.actions
 
 export default productSlice.reducer
