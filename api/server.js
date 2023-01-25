e34fe15f1346eb8cc24400431343b29fbf452755
Adedipupo <ajokanola@terragonltd.com>
2023-01-25 09:42:04 +0100

    logger

--- api/server.js
+++ api/server.js
@@ -1,61 +1,67 @@
-import express, { urlencoded } from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import mongoose from 'mongoose';
-import bodyParser from 'body-parser';
-import productRoutes from './routes/productRoute.js';
-import userRoutes from './routes/userRoute.js';
-import contactRoutes from './routes/contactRoute.js';
-import errorHandler from './middleware/errorMiddleware.js';
-import cookieParser from 'cookie-parser';
-import morgan from 'morgan';
-import path from 'path';
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
+import express, { urlencoded } from 'express'
+import dotenv from 'dotenv'
+import cors from 'cors'
+import mongoose from 'mongoose'
+import bodyParser from 'body-parser'
+import productRoutes from './routes/productRoute.js'
+import userRoutes from './routes/userRoute.js'
+import contactRoutes from './routes/contactRoute.js'
+import errorHandler from './middleware/errorMiddleware.js'
+import cookieParser from 'cookie-parser'
+import morgan from 'morgan'
+import path from 'path'
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
 
 dotenv.config()
 
 const app = express()
 
 app.use(express.json())
-app.use(urlencoded({extended:false}))
+app.use(urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(cookieParser())
-app.use(cors({
-  origin: ["https://inventoryapp-jf7i.vercel.app","http://localhost:3000","https://invent-jb0qpaptr-adedipupo.vercel.app"],
-  optionsSuccessStatus: 200,
-  credentials: true,
-}));
-if (process.env.NODE_ENV === "development") {
+app.use(
+  cors({
+    origin: [
+      'https://inventoryapp-jf7i.vercel.app',
+      'http://localhost:3000',
+      'https://invent-jb0qpaptr-adedipupo.vercel.app',
+    ],
+    optionsSuccessStatus: 200,
+    credentials: true,
+  }),
+)
+if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'))
+} else {
+  app.use(logger('combined'))
 }
 
-
-
 // const __dirname = path.resolve();
-const __dirname = dirname(fileURLToPath(import.meta.url));
-app.use("/uploads",express.static(path.join(__dirname,"uploads")))
+const __dirname = dirname(fileURLToPath(import.meta.url))
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
 
 const PORT = process.env.PORT || 1234
 
-app.get('/',(req, res)=>{
-  res.send("api is live...")
-});
-app.get('/health',(req, res)=>{
-  res.send("api is live and fine oooo...")
-});
-app.use('/api/users',userRoutes);
-app.use('/api/products',productRoutes);
-app.use('/api/contact',contactRoutes);
+app.get('/', (req, res) => {
+  res.send('api is live...')
+})
+app.get('/health', (req, res) => {
+  res.send('api is live and fine oooo...')
+})
+app.use('/api/users', userRoutes)
+app.use('/api/products', productRoutes)
+app.use('/api/contact', contactRoutes)
 app.use(errorHandler)
-
-
 
 mongoose
   .connect(process.env.MONGODB_URI)
-  .then(() => {app.listen(PORT, () => {
-    console.log(`Connected to mongodb successfully,Server running on port ${PORT}`)
-  })})
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(
+        `Connected to mongodb successfully,Server running on port ${PORT}`,
+      )
+    })
+  })
   .catch((err) => console.log(err))
-
-
